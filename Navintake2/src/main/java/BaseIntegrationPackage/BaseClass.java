ce9566c60b64dfaa1d31ae569be776c243d72e39
pnavi <pnavi@192.168.1.116>
2022-03-21 00:50:42 +0000

    First commit

---
+++ Navintake2/src/main/java/BaseIntegrationPackage/BaseClass.java
@@ -0,0 +1,31 @@
+package BaseIntegrationPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+
+	public BaseClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//chromedriver.exe->invokes chrome driver
+		//webdriver.chrome.driver is a property-> value of path
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium learning\\ChromeDriver\\chromedriver.exe");
+		//WebDriver is an interface which defines driver methods & ChromeDriver implements it using overriding
+		WebDriver driver = new ChromeDriver();
+		//get method to launch a URL
+		driver.get("https://rahulshettyacademy.com");
+		//driver.close();
+		 
+	}
+
+}
+//GitHub
+//Step 1: Create account and sign in
+//Step 2: Create a project and a repository
+//Step 3: Start Eclipse
+//Step 4: Goto Perspective -> Git Repositories and click on Clone Git repositories
+//Step 5: Keep your Git repo as public to clone a repo (maybe because of free version)
