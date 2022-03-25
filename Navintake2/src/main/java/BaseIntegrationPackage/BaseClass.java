aaf1357e427514f82df63c4240a8770d6f968a0f
pnavi <pnavi@192.168.1.116>
2022-03-25 15:43:39 +0000

    Locators learning

--- Navintake2/src/main/java/BaseIntegrationPackage/BaseClass.java
+++ Navintake2/src/main/java/BaseIntegrationPackage/BaseClass.java
@@ -1,5 +1,8 @@
 package BaseIntegrationPackage;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,15 +12,23 @@ public class BaseClass {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IllegalMonitorStateException {
 		
 		//chromedriver.exe->invokes chrome driver
 		//webdriver.chrome.driver is a property-> value of path
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium learning\\ChromeDriver\\chromedriver.exe");
 		//WebDriver is an interface which defines driver methods & ChromeDriver implements it using overriding
 		WebDriver driver = new ChromeDriver();
+		//declare an implicit wait for all its operations
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		//get method to launch a URL
-		driver.get("https://rahulshettyacademy.com");
+		driver.get("https://sso.teachable.com/secure/9521/identity/login");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getWindowHandles());
+		driver.findElement(By.id("email")).sendKeys("navinkumar");
+		driver.findElement(By.className("btn-primary")).click();
+		//driver.wait(3000); don't use doesn't work
 		//driver.close();
 		 
 	}
@@ -35,3 +46,11 @@ public class BaseClass {
 //Step 9: If password doesn't work, then goto Github -> user settings -> Developer settings
 	// -> Personal access tokens -> Generate new token -> copy token -> Paste in Eclipse commit
 	// as password
+
+
+//css syntax
+//tagname.classname, tagname#id, tagname[attribute='value']
+//selectorshub - type css selector
+//or in console put - $('css')
+//xpath syntax
+//
